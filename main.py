8a5cad8af518fbd0714c2eb0067c7ea98329736b
RITTUL RAJ <rittulraj@gmail.com>
2025-04-03 23:49:14 +0530

    basic data preprocessing

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+# %%
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error, r2_score
+data = pd.read_csv("./AXISBANK.csv")
+data.head()
 
