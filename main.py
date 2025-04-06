b411ff6de11a71b0156178f277340ff1e07292ec
RITTUL RAJ <rittulraj@gmail.com>
2025-04-06 20:13:03 +0530

    plotted some pair plot for visualistaion

--- main.py
+++ main.py
@@ -21,3 +21,45 @@ data.drop(columns=["Trades",'Deliverable Volume',"%Deliverble"],inplace=True)
 # %%
 data.isnull().sum()
 
+# %%
+
+
+# %%
+sns.boxplot(x=data['Prev Close'])
+
+# %%
+sns.boxplot(x=data['Close'])
+
+
+# %%
+q1 = data['Close'].quantile(0.25)
+q2 = data['Close'].quantile(0.75)
+iqr = q2 - q1
+min_range = q1 - (1.5*iqr)
+max_range = q2 + (1.5*iqr)
+data = data[data['Close']<max_range]
+data.head()
+
+# %%
+sns.boxplot(x=data['Close'])
+
+# %%
+data['Date'] = pd.to_datetime(data['Date'])
+
+
+# %%
+data.head()
+
+# %%
+# sns.heatmap(data.corr(),annot=True,cmap="Blues")
+numeric_data = data.select_dtypes(include=['number'])
+sns.heatmap(numeric_data.corr(), annot=True, cmap="Blues", linewidths=0.5)
+# %%
+
+
+# %%
+sns.pairplot(data[['Close', 'Volume', 'Open', 'High']])
+plt.show()
+
+# %%
+
